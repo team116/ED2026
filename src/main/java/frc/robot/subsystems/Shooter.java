2fc3766199cb0e25af0cd08e45cc4164e2ff459e
ftc519 <>
2026-02-17 14:47:35 -0500

    Added stub for Shooter subsystem. - Matthew Wacek

---
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -0,0 +1,42 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.PersistMode;
+import com.revrobotics.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+
+import frc.robot.stubs.DummyMotorController;
+import frc.robot.Constants;
+
+public class Shooter implements Subsystem {
+    private final MotorController leftShooterMotor;
+    private final MotorController rightShooterMotor;
+
+    private final SparkMaxConfig leftShooterConfig = new SparkMaxConfig();
+    private final SparkMaxConfig rightShooterConfig = new SparkMaxConfig();
+
+    public Shooter() {
+        if(Constants.BehaviorConstants.USE_STUBS) {
+            leftShooterMotor = new DummyMotorController();
+            rightShooterMotor = new DummyMotorController();
+        } else {
+            SparkMax leftShooterMotor = new SparkMax(Constants.HardwareIDConstants.LEFT_SHOOTER_MOTOR_ID, MotorType.kBrushless);
+            SparkMax rightShooterMotor = new SparkMax(Constants.HardwareIDConstants.RIGHT_SHOOTER_MOTOR_ID, MotorType.kBrushless);
+
+            leftShooterMotor.configure(leftShooterConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+            rightShooterMotor.configure(rightShooterConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+            this.leftShooterMotor = leftShooterMotor;
+            this.rightShooterMotor = rightShooterMotor;
+        }
+    }
+
+    public void run(double speed) {
+        leftShooterMotor.set(speed);
+        rightShooterMotor.set(speed);
+    }
+}
